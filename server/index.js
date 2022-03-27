f9f734257578d28acb18fa6e137efe334bb586b4
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-03-27 02:51:20 +0200

    added basic controllers

--- server/index.js
+++ server/index.js
@@ -3,15 +3,19 @@ const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
 const corse = require('cors');
+const fileUpload = require('express-fileupload');
 const router = require('./routes/index');
 
 //middleware хендлер для обработки ошибок
 const errorHandler = require('./middleware/ErrorHandlingMiddleware.js');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(corse());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
 
 //Обработка ошибок всегда идёт в конце
